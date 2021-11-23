c1c70303377710365323a526ffdd2b04b5cf753f
TokmurzinIlia <ilia.tokmurzin@gmail.com>
2021-11-23 11:20:31 +0300

    Commit

--- src/main/java/com/it_academy/practice/junit_basics/CustomArgumentProviderSubtraction.java
+++ src/main/java/com/it_academy/practice/junit_basics/CustomArgumentProviderSubtraction.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 import java.util.stream.Stream;
 
 public class CustomArgumentProviderSubtraction implements ArgumentsProvider {
+
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
         return Stream.of(
